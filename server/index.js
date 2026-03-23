d6d865db274a71962561dfc72fdef79cb2ed315e
Ayush Pratap <ayushprince110@gmail.com>
2026-03-24 04:54:41 +0530

    connected frontend to remote server and also fixed CORS issue in backend

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ const adminRoutes = require('./routes/adminRoutes');
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:5000', 'https://rheel.vercel.app', 'https://rheel.onrender.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 const path = require('path');
